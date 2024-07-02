506a6662a635161700ba43784278eea4f946cbd5
Theeramate <theeramate0830@gmail.com>
2024-07-02 14:00:11 +0700

    fix

--- include/cfg/config.h
+++ include/cfg/config.h
@@ -1,16 +1,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define WIFI_SSID   "CUHomeWiFi(2.4G)-0818"
-#define WIFI_PWD    "430185539"
-
-// #define WIFI_SSID "NDRS-Wongsawang-5G"
-// #define WIFI_PWD "ndrs_2010"
-
-#define MQTT_SERVER "188.166.205.17"
-#define MQTT_PORT 1884
-#define MQTT_ACCESS_TOKEN "n7cP1kz1ksw80GNcHxcM"
-
-static const int firmware_chunk_size = 1024;
+static char WIFI_SSID_S[100];
+static char WIFI_PWD_S[100];
+static char MQTT_SERVER_S[100];
+static int MQTT_PORT_S;
+static char MQTT_TOKEN_S[100];
 
 #endif 
